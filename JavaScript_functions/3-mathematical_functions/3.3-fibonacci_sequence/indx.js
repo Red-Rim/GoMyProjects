dbd391bf88e7cd76e6f1275b4f59109e9b7a5757
Red-Rim <salmaouakhir20@gmail.com>
2024-12-09 00:39:44 +0100

    filter functions

---
+++ JavaScript_functions/3-mathematical_functions/3.3-fibonacci_sequence/indx.js
@@ -0,0 +1,15 @@
+function fibonacci(n) {
+   let x = 0, y = 1;
+   let result = [];
+
+   for (let i = 0; i < n; i++) {
+       result.push(x); 
+       let next = x + y;
+       x = y;
+       y = next;
+   }
+
+   return result;
+}
+
+console.log(fibonacci(5));
